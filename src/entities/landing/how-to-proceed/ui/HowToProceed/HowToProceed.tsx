937cc17bc24c6a3d69bdf0270b62c527a9ce25b2
mtoktaliev <mtoktalievmegacom@gmail.com>
2026-05-07 17:24:15 +0600

    Завершил секцию Как поступить в вуз. Добавил фотографии в карточки

--- src/entities/landing/how-to-proceed/ui/HowToProceed/HowToProceed.tsx
+++ src/entities/landing/how-to-proceed/ui/HowToProceed/HowToProceed.tsx
@@ -1,15 +1,20 @@
 import { Button } from "antd";
 import { useTranslation } from "~shared/lib/i18n";
-import smartphone from "~shared/assets/smartphone.png";
+import registerImage from "~shared/assets/registration.png";
+import sendImage from "~shared/assets/send.png";
+import registerImageWhite from "~shared/assets/registrationWhite.svg";
+import targetImage from "~shared/assets/target.png";
+import checkImage from "~shared/assets/check.png";
 import { useTheme } from "~shared/lib/theme";
 import { VideoPlayer } from "~features/shared/video-player";
+import { AltArrowRight } from "@solar-icons/react-perf/Outline";
 
 export const HowToProceed: React.FC = () => {
   const { t } = useTranslation();
   const { theme } = useTheme();
 
   return (
-    <div className="bg-white dark:bg-slate-950">
+    <>
       <div className="container mx-auto px-6 flex flex-col gap-6 py-20">
         <div className="grid grid-cols-12">
           <div className="col-span-8 col-start-3 text-center flex flex-col gap-6">
@@ -22,44 +27,77 @@ export const HowToProceed: React.FC = () => {
           </div>
         </div>
         <div className="grid grid-cols-12 gap-6">
-          <div className="col-span-3 p-6 pb-0 rounded-2xl bg-slate-100 dark:bg-slate-900 flex flex-col gap-6 min-h-115">
-            <div className="flex flex-col gap-6">
-              <h3 className="font-semibold text-center text-xl dark:text-white">
+          <div className="col-span-3 p-12 rounded-2xl bg-white dark:bg-slate-950 flex flex-col gap-6">
+            <div className="flex flex-col gap-12">
+              <h3 className="font-semibold text-center text-lg dark:text-white">
                 {t("landing:howToProceedStep1")}
               </h3>
+
               <div className="flex justify-center">
-                <img src={smartphone} alt="hero" />
+                <img src={registerImage} alt="hero" />
               </div>
+              <Button
+                type="link"
+                size="large"
+                icon={<AltArrowRight />}
+                iconPlacement="end"
+              >
+                Узнать подробнее
+              </Button>
             </div>
           </div>
-          <div className="col-span-3 p-6 pb-0 rounded-2xl bg-slate-100 dark:bg-slate-900 flex flex-col gap-6 min-h-115">
-            <div className="flex flex-col gap-6">
-              <h3 className="font-semibold text-center text-xl dark:text-white">
+          <div className="col-span-3 p-12 pb-0 rounded-2xl bg-white dark:bg-slate-950 flex flex-col gap-6">
+            <div className="flex flex-col gap-12">
+              <h3 className="font-semibold text-center text-lg dark:text-white">
                 {t("landing:howToProceedStep2")}
               </h3>
               <div className="flex justify-center">
-                <img src={smartphone} alt="hero" />
+                <img src={targetImage} alt="hero" />
               </div>
+              <Button
+                type="link"
+                size="large"
+                icon={<AltArrowRight />}
+                iconPlacement="end"
+              >
+                Узнать подробнее
+              </Button>
             </div>
           </div>
-          <div className="col-span-3 p-6 pb-0 rounded-2xl bg-slate-100 dark:bg-slate-900 flex flex-col gap-6 min-h-115">
-            <div className="flex flex-col gap-6">
-              <h3 className="font-semibold text-center text-xl dark:text-white">
+          <div className="col-span-3 p-12 rounded-2xl bg-white dark:bg-slate-950 flex flex-col gap-6">
+            <div className="flex flex-col gap-12">
+              <h3 className="font-semibold text-center text-lg dark:text-white">
                 {t("landing:howToProceedStep3")}
               </h3>
               <div className="flex justify-center">
-                <img src={smartphone} alt="hero" />
+                <img src={sendImage} alt="hero" />
               </div>
+              <Button
+                type="link"
+                size="large"
+                icon={<AltArrowRight />}
+                iconPlacement="end"
+              >
+                Узнать подробнее
+              </Button>
             </div>
           </div>
-          <div className="col-span-3 p-6 pb-0 rounded-2xl bg-slate-100 dark:bg-slate-900 flex flex-col gap-6 min-h-115">
-            <div className="flex flex-col gap-6">
-              <h3 className="font-semibold text-center text-xl dark:text-white">
+          <div className="col-span-3 p-12 rounded-2xl bg-white dark:bg-slate-950 flex flex-col">
+            <div className="flex flex-col gap-12">
+              <h3 className="font-semibold text-center text-lg dark:text-white">
                 {t("landing:howToProceedStep4")}
               </h3>
               <div className="flex justify-center">
-                <img src={smartphone} alt="hero" />
+                <img src={checkImage} alt="hero" />
               </div>
+              <Button
+                type="link"
+                size="large"
+                icon={<AltArrowRight />}
+                iconPlacement="end"
+              >
+                Узнать подробнее
+              </Button>
             </div>
           </div>
         </div>
@@ -82,6 +120,6 @@ export const HowToProceed: React.FC = () => {
           />
         </div>
       </div>
-    </div>
+    </>
   );
 };
